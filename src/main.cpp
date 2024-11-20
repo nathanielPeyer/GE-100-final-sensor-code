b06025fd7efe0c06082ef4eff8bba6e2ce9a1956
Nathaniel Peyer <Nate.Peyer@valpo.edu>
2024-11-20 08:34:28 -0600

    All checks are now implemented but not checked, and OLED display integration needed.

--- src/main.cpp
+++ src/main.cpp
@@ -17,34 +17,38 @@ const int BLUELED = 12;
 const int REDLED = 11;
 const int TAPSENSOR = 1;
 
-int timeInner1 = 0;
-int timeInner2 = 0;
+int timeInner = 0;
 int timeOuter = 0;
+int darkness = 0;
+int tapState;
 
+int darknessStorage[180];
+
+long int intialTempuDF = 0;
 long int tempSensoruVolts;
 long int tempSensoruDC;
 long int tempSensoruDF;
-long int intialTempuDF = 0;
 long int tempAvg;
 long int photoSensorVolt;
 long int photoSensoruVolt;
 long int touchSensorVolts;
-long int tapState;
+
 
 long int tempDFStorage[18];
 long int tempDFTempStorage[10];
 
-bool isDark;
 bool isLoad;
 bool isVibration;
 bool isLoad1;
 bool isLoad2;
 
-bool isDarkStorage[180];
 bool isLoadStorage[180];
 bool isVibrationStorage[180];
 
-
+char loadDetected[50];
+char vibrationDetected[50];
+char tempOut[100];
+char isDark[30];
 
 void setup() 
 {
@@ -71,13 +75,18 @@ void loop()
   //resetting lights
   digitalWrite(YELLOWLED, LOW);
   digitalWrite(BLUELED, LOW);
+
+  //resetting some variables
+  darkness = 4;
+  isLoad = false;
+  isVibration = false;
   
   //shift data in storage arrays so there is an open slot.
   if(timeOuter == 180)
   {
     for(int i = 1; i < 180; i++)
     {
-      isDarkStorage[i-1] = isDarkStorage[i];
+      darknessStorage[i-1] = darknessStorage[i];
       isLoadStorage[i-1] = isLoadStorage[i];
       isVibrationStorage[i-1]= isVibrationStorage[i];
     }
@@ -96,7 +105,7 @@ void loop()
   tempSensoruVolts = (long)analogRead(A0);
   photoSensorVolt = (long)analogRead(A1);
   touchSensorVolts = (long)analogRead(A2);
-  tapState = (long)digitalRead(TAPSENSOR);
+  tapState = digitalRead(TAPSENSOR);
 
   //conversions
   tempSensoruVolts = tempSensoruVolts*(5000000/1024);
@@ -108,27 +117,34 @@ void loop()
   //checking if there is a load
   if(touchSensorVolts < 2.5*MICRO)
   {
-    timeInner1++;
-    if(timeInner1 >= 6)
+    timeInner++;
+    
+    
+
+    if(timeInner >= 6)
     {
-      timeInner1 = 1;
+      timeInner = 1;
       isLoad1 = false;
       isLoad2 = false;
     }
+    
     // defining load as true.
     //storing values for 3 minutes before being written over.
     isLoad = true;
+    sprintf(loadDetected, "A load has been detected near or on the bridge!");
     isLoadStorage[timeOuter]= isLoad;
     //checks for second load
+    
     if(isLoad1 == false)
     {
       isLoad1 = true;
+
     }
     else
     {
       //if found triggers yellow led
       isLoad2 = true;
-      timeInner1 = 10;
+      timeInner = 10;
       digitalWrite(YELLOWLED, HIGH);
     }
   }
@@ -138,8 +154,10 @@ void loop()
     isLoad = false;
     isLoadStorage[timeOuter] = isLoad;
   }
+  
   if(timeOuter%10 == 0)
   {
+    tempAvg = 0;
     tempDFStorage[timeOuter] = tempSensoruDF;
     if(intialTempuDF == 0)
     {
@@ -154,12 +172,63 @@ void loop()
       tempAvg = tempAvg + tempDFStorage[i];
     }
     tempAvg = tempAvg/10;
+    sprintf(tempOut, "The current tempature is %ld.%06ld F, The average temperature over the last ten seconds is %ld.%06ld F.",tempSensoruDF/MICRO, tempSensoruDF%MICRO, tempAvg/MICRO, tempAvg%MICRO);
   }
   else
   {
     tempDFStorage[timeOuter] = tempSensoruDF;
   }
   
+  /*
+    Darkness scale
+    0 - day
+    1 - semi-dark/dusk
+    2 - dark
+    4 - default state
+  */
+  
+  //assigning values to darkness levels
+  if(photoSensoruVolt > 2.2*MICRO)
+  {
+    darkness = 0;
+  }
+  else if( photoSensoruVolt < 1.0*MICRO)
+  {
+    darkness = 1;
+  }
+  else
+  {
+    darkness = 2;
+  }
+  darknessStorage[timeOuter] = darkness;
+  
+  //checking if it went from day to dusk.
+  if(timeOuter > 0)
+  {
+    if(((darknessStorage[timeOuter]+darknessStorage[timeOuter-1]) == 1) || ((darknessStorage[timeOuter]-darknessStorage[timeOuter-1]) == 0))
+    {      
+      digitalWrite(REDLED, HIGH);
+    }
+  }
+  
+  //checking if completely dark and if printing message
+  if(darknessStorage[timeOuter] == 2)
+  {
+    sprintf(isDark, "It is dark by the bridge!");
+  }
+
+  //checking for load using sensor number 2
+  if(tapState == HIGH)
+  {
+    sprintf(vibrationDetected, "A load has been detected by sensor number 2.");
+    isVibration = true;
+  }
+  else
+  {
+    isVibration = false;
+  }
+  isVibrationStorage[timeOuter] = isVibration;
+
   //waiting a second before going through loop again.
   timeOuter++;
   delay(1000);
